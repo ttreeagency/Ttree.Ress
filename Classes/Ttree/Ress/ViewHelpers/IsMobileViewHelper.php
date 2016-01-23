0e1e18bff09bc69fccfb1aa590e456a721a9668b
Thierry Brodard <thierry.brodard@techniconcept.ch>
2016-01-23 18:43:44 +0100

    FEATURE: Add ress:isMobile ViewHelper

---
+++ Classes/Ttree/Ress/ViewHelpers/IsMobileViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+namespace Ttree\Ress\ViewHelpers;
+
+/*
+ * This script belongs to the TYPO3 Flow package "Ttree.Ress".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the MIT licence.                                          *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+use Detection\MobileDetect;
+
+
+class IsMobileViewHelper extends AbstractConditionViewHelper
+{
+
+    /**
+     * @var MobileDetect
+     */
+    protected $mobileDetection = NULL;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->mobileDetection = new MobileDetect();
+
+    }
+
+    public function render()
+    {
+        if ($this->mobileDetection->isMobile()) {
+            return $this->renderThenChild();
+        } else {
+            return $this->renderElseChild();
+        }
+    }
+}
